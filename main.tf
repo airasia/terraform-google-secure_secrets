c2e57061a2df71c2b50583cbc50d8832c9434541
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-08-30 06:21:48 +0800

    Remove deprecated use of nested providers

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
 }
 
-provider "google" {
-  version = ">= 3.13.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
-}
-
-provider "google-beta" {
-  version = ">= 3.13.0" # see https://github.com/terraform-providers/terraform-provider-google-beta/releases
-}
-
 data "google_client_config" "google_client" {}
 
 data "google_kms_secret" "deciphered_secrets" {
